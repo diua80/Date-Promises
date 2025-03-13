c87e8825e5b3da284bae8e1c1b1e4c9adc9481b9
Iryna Yatsenko <iryna.didova@gmail.com>
2025-03-13 15:39:32 +0200

    the color change task is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+refs = {
+    startBtn: document.querySelector("[data-start]"),
+    stopBtn: document.querySelector("[data-stop]"),
+}
+// console.log(refs.startBtn);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+refs.startBtn.addEventListener("click", startToChangeColor);
+refs.stopBtn.addEventListener("click", stopToChangeColor);
+let intervalId = null;
+
+function stopToChangeColor() {
+    clearInterval(intervalId);
+    refs.startBtn.style.disabled = false;
+};
+
+function startToChangeColor() {
+    refs.startBtn.style.disabled = true;
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}

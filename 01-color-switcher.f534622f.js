4652b1af8f8c7b23117bf76192043ab8724ae274
diua80 <119079479+diua80@users.noreply.github.com>
2025-03-13 13:39:54 +0000

    Deploying to gh-pages from @ diua80/Date-Promises@c87e8825e5b3da284bae8e1c1b1e4c9adc9481b9 🚀

---
+++ 01-color-switcher.f534622f.js
@@ -0,0 +1,2 @@
+refs={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},refs.startBtn.addEventListener("click",(function(){refs.startBtn.style.disabled=!0,t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),refs.stopBtn.addEventListener("click",(function(){clearInterval(t),refs.startBtn.style.disabled=!1}));let t=null;
+//# sourceMappingURL=01-color-switcher.f534622f.js.map
